083f80754b7729ac1b72da3fe05c7dd43d6f77f2
Daimon22 <subway.to.sally@gmx.de>
2008-12-22 09:07:43 +0100

    fix compile issue with php-templates, fix config.xml

--- templates/tmpl_lib.c
+++ templates/tmpl_lib.c
@@ -214,7 +214,7 @@ zval			**ztag;
 	buf_alloc = TMPL_MAX_TAG_LEN+4;
 	buf = (uchar*)emalloc(buf_alloc);
 	/* Searching algorythm will require larger buffer */
-	ZV(tmpl->original) = (char*)erealloc(ZV(tmpl->original), 
+	(char*)ZV(tmpl->original) = erealloc(ZV(tmpl->original), 
 		ZL(tmpl->original) + MAX(
 			ZL(tmpl->tag_left), MAX(
 				ZL(tmpl->tag_right), MAX(
@@ -697,7 +697,7 @@ ulong		key_tag_index;
 					/* Make a recursive call */
 					if(buf_alloc <= (unsigned)(ZL(path)+1+ZL(tag->name)+1)) {
 						while(buf_alloc <= (unsigned)(ZL(path)+1+ZL(tag->name)+1)) buf_alloc <<= 1;
-						ZV(new_path) = (char*)erealloc(ZV(new_path), buf_alloc);
+						(char*)ZV(new_path) = erealloc(ZV(new_path), buf_alloc);
 					}
 					sprintf(ZV(new_path), (1 == ZL(path) && '/' == ZV(path)[0]) ? "%s%s" : "%s/%s", ZV(path), ZV(tag->name));
 					ZL(new_path) = ZL(path) + ZL(tag->name) + ((1 == ZL(path) && '/' == ZV(path)[0]) ? 0 : 1);
@@ -877,7 +877,7 @@ int				tag_max, tag_cur;
 			} else p = key;
 			ZVAL_STRINGL(new_value, p, (TMPL_LONG == mod) ? key_len-1 : key_len-1-(p-key), 1);
 			if((TMPL_TAG | TMPL_CONTEXT) == mask && TMPL_CONTEXT == tag->typ) {
-				ZV(new_value) = (char*)erealloc(ZV(new_value), ZL(new_value)+2);
+				(char*)ZV(new_value) = erealloc(ZV(new_value), ZL(new_value)+2);
 				memcpy(ZV(new_value)+ZL(new_value), "/", 2);
 				ZL(new_value) += 1;
 			}
